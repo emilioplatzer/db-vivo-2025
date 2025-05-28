33aa2740e6840ac561fbbca03c3c8ccee7cbca58
Emilio Platzer <emilioplatzer@gmail.com>
2025-05-28 20:48:14 -0300

    Arreglo la seguridad agrego los usuarios de db

--- src/db/db-creation.sql
+++ src/db/db-creation.sql
@@ -1,1 +1,7 @@
-create database inscriptos;
+-- create database inscriptos;
+--- correr separado
+create user inscriptos_owner;
+alter database inscriptos  owner to inscriptos_owner;
+
+create user inscriptos_user;
+grant connect on database inscriptos to inscriptos_user;
