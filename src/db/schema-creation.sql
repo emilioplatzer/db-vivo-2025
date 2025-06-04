66fd3f54a20dadd0c680bfe70e88f63a9dccefdc
Emilio Platzer <emilioplatzer@gmail.com>
2025-06-04 19:36:09 -0300

    Creamos la tabla de materias con un ejemplo

--- src/db/schema-creation.sql
+++ src/db/schema-creation.sql
@@ -4,6 +4,7 @@ drop table if exists alumnos;
 drop schema if exists insc;
 
 create schema insc;
+grant usage on schema insc to inscriptos_user; 
 
 set search_path = insc;
 
@@ -14,6 +15,19 @@ create table alumnos(
     mail text,
     annio_ingreso integer
 );
+grant select, insert on alumnos to inscriptos_user;
 
-grant usage on schema insc to inscriptos_user; 
+create table materias(
+    mat varchar(10) primary key,
+    nombre text,
+    area text,
+    horas integer
+);
+
+insert into materias
+    (mat      , nombre                                    , area        , horas) values
+    ('BD'     , 'Base de Datos'                           , 'Sistemas'  ,     7),
+    ('MN'     , 'Métodos Numéricos'                       , 'Algoritmos',     5),
+    ('AED3'   , 'Algoritmos y Estructuras de Datos III'   , 'Algoritmos',     5);
+
 grant select, insert on alumnos to inscriptos_user;
